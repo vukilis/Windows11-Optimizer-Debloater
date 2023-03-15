e3d45e9001f7c72a8b7ee1a04b157a692a92fd00
vukilis <vuklekic22@gmail.com>
2023-03-15 16:49:58 +0100

    commit

--- main.ps1
+++ main.ps1
@@ -104,6 +104,8 @@ function Invoke-Button {
         "wpf_FixesUpdate" {Invoke-FixesUpdate}
         "wpf_Updatesdisable" {Invoke-UpdatesDisable}
         "wpf_Updatessecurity" {Invoke-UpdatesSecurity}
+        "wpf_FeatureInstall" {Invoke-FeatureInstall}
+        "wpf_PanelAutologin" {Invoke-PanelAutologin}
     }
 }
 
@@ -1498,7 +1500,68 @@ switch ($Panel){
     "wpf_PanelDiskManagement"       {cmd /c diskmgmt.msc}
 }
 }
+function Invoke-FeatureInstall {
+    <#
+    .DESCRIPTION
+    GUI Function to install Windows Features
+    #>
+    If ( $wpf_FeaturesDotnet.IsChecked -eq $true ) {
+        Enable-WindowsOptionalFeature -Online -FeatureName "NetFx4-AdvSrvs" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All -NoRestart
+    }
+    If ( $wpf_FeaturesHyperv.IsChecked -eq $true ) {
+        Enable-WindowsOptionalFeature -Online -FeatureName "HypervisorPlatform" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Tools-All" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Management-PowerShell" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Hypervisor" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Services" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Management-Clients" -All -NoRestart
+        cmd /c bcdedit /set hypervisorschedulertype classic
+        Write-Host "HyperV is now installed and configured. Please Reboot before using."
+    }
+    If ( $wpf_FeaturesLegacymedia.IsChecked -eq $true ) {
+        Enable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "MediaPlayback" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "DirectPlay" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "LegacyComponents" -All -NoRestart
+    }
+    If ( $wpf_FeatureWsl.IsChecked -eq $true ) {
+        Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" -All -NoRestart
+        Write-Host "WSL is now installed and configured. Please Reboot before using."
+    }
+    If ( $wpf_FeatureNfs.IsChecked -eq $true ) {
+        Enable-WindowsOptionalFeature -Online -FeatureName "ServicesForNFS-ClientOnly" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "ClientForNFS-Infrastructure" -All -NoRestart
+        Enable-WindowsOptionalFeature -Online -FeatureName "NFS-Administration" -All -NoRestart
+        nfsadmin client stop
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ClientForNFS\CurrentVersion\Default" -Name "AnonymousUID" -Type DWord -Value 0
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ClientForNFS\CurrentVersion\Default" -Name "AnonymousGID" -Type DWord -Value 0
+        nfsadmin client start
+        nfsadmin client localhost config fileaccess=755 SecFlavors=+sys -krb5 -krb5i
+        Write-Host "NFS is now setup for user based NFS mounts"
+    }
+    $ButtonType = [System.Windows.MessageBoxButton]::OK
+    $MessageboxTitle = "All features are now installed "
+    $Messageboxbody = ("Done")
+    $MessageIcon = [System.Windows.MessageBoxImage]::Information
 
+    [System.Windows.MessageBox]::Show($Messageboxbody, $MessageboxTitle, $ButtonType, $MessageIcon)
+
+    Write-Host "================================="
+    Write-Host "---  Features are Installed   ---"
+    Write-Host "================================="
+}
+function Invoke-PanelAutologin {
+    <#
+        .DESCRIPTION
+        PlaceHolder
+    #>
+    curl.exe -ss "https://live.sysinternals.com/Autologon.exe" -o $env:temp\autologin.exe # Official Microsoft recommendation https://learn.microsoft.com/en-us/sysinternals/downloads/autologon
+    cmd /c $env:temp\autologin.exe
+}
 ########################################### /CONFIG ########################################### 
 Get-Author
 $wpf_diskNameInfo.Add_SelectionChanged({Get-DiskInfo})
