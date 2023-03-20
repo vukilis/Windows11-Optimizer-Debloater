fcfbdecd3fc93638049a928f935b8abfa1fdfae5
vukilis <vuklekic22@gmail.com>
2023-03-20 15:31:26 +0100

    commit

--- main.ps1
+++ main.ps1
@@ -17,13 +17,13 @@ $inputXAML=$inputXAML -replace 'mc:Ignorable="d"', '' -replace 'x:N', "N" -repla
 [XML]$XAML=$inputXAML
 
 # Check if chocolatey is installed and get its version
-# if ((Get-Command -Name choco -ErrorAction Ignore) -and ($chocoVersion = (Get-Item "$env:ChocolateyInstall\choco.exe" -ErrorAction Ignore).VersionInfo.ProductVersion)) {
-#     Write-Output "Chocolatey Version $chocoVersion is already installed"
-# }else {
-#     Write-Output "Seems Chocolatey is not installed, installing now"
-#     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-#     powershell choco feature enable -n allowGlobalConfirmation
-# }
+if ((Get-Command -Name choco -ErrorAction Ignore) -and ($chocoVersion = (Get-Item "$env:ChocolateyInstall\choco.exe" -ErrorAction Ignore).VersionInfo.ProductVersion)) {
+    Write-Output "Chocolatey Version $chocoVersion is already installed"
+}else {
+    Write-Output "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    powershell choco feature enable -n allowGlobalConfirmation
+}
 
 $reader = New-Object System.Xml.XmlNodeReader $XAML
 try {
