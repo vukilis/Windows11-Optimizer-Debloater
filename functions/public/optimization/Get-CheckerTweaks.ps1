99042f98728f3dca082a350f796b1a4cfaf42cb6
vukilis <vuklekic22@gmail.com>
2024-01-09 16:45:59 +0100

    checker for new tweak

--- functions/public/optimization/Get-CheckerTweaks.ps1
+++ functions/public/optimization/Get-CheckerTweaks.ps1
@@ -9,11 +9,14 @@ function Get-CheckerTweaks{
     $b = $wpf_ToggleNumLock.IsChecked = Get-ToggleValue -Path 'HKCU:\Control Panel\Keyboard' -Name 'InitialKeyboardIndicators'
     $c = $wpf_ToggleExt.IsChecked = (Get-ToggleValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt') -eq 0
     $d = $wpf_ToggleMouseAcceleration.IsChecked = (Get-ToggleValue -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseSpeed') -and (Get-ToggleValue -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseThreshold1') -and (Get-ToggleValue -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseThreshold2')
-    $getValue = Get-ItemPropertyValue 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Hidden'
-    $e = $wpf_ToggleHiddenFiles.IsChecked = $(If ($getValue -eq 0) {$false} Else {$true})
+    $getE = Get-ItemPropertyValue 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Hidden'
+    $e = $wpf_ToggleHiddenFiles.IsChecked = $(If ($getE -eq 0) {$false} Else {$true})
     $f = $wpf_ToggleSearch.IsChecked = (Get-ToggleValue -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search' -Name 'SearchBoxTaskbarMode') -eq 0
     $g = $wpf_TogglefIPv6.IsChecked = $(If ((Get-NetAdapterBinding -Name 'Ethernet' -ComponentID ms_tcpip6).Enabled -eq "True" -And $(Get-NetAdapterBinding -Name 'Ethernet' -ComponentID ms_tcpip6).Enabled -eq "False") {$true} Else {$false})
+    $getH = Get-ItemPropertyValue 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'EnableSnapAssistFlyout'
+    $h = $wpf_ToggleSnapLayouts.IsChecked = $(If ($getH -eq 0) {$true} Else {$false})
+    $i = $wpf_ToggleVerboseLogon.IsChecked = Get-ToggleValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'VerboseStatus'
 
-    return $a -and $b -and $c -and $d -and $e -and $f -and $g
+    return $a -and $b -and $c -and $d -and $e -and $f -and $g -and $h -and $i
 }
 Get-CheckerTweaks | out-null
