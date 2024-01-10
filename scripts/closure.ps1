c37a8e393b4f53f7b7feb737ced8df6296b1da7d
vukilis <vuklekic22@gmail.com>
2024-01-11 00:49:32 +0100

    check elevation

--- scripts/closure.ps1
+++ scripts/closure.ps1
@@ -8,6 +8,14 @@ if ($psVersion.Major -eq 7 -and $psVersion.Minor -ge 1) {
 } else {
     Write-Host "You are running a different version of PowerShell."
 }
+
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "################################################################################################" -ForegroundColor Red
+    Write-Host "Not running as administrator. Please run the script as an administrator." -ForegroundColor Red
+    Write-Host "If you continue to use as non-admin user, it will result to script creates unexpected behaviour!" -ForegroundColor Red
+    Write-Host "################################################################################################" -ForegroundColor Red
+} 
+
 $wpf_diskNameInfo.Add_SelectionChanged({Get-DiskInfo})
 $wpf_diskName.Add_SelectionChanged({Get-DiskSize})
 $wpf_ddlServices.Add_SelectionChanged({Get-Services})
