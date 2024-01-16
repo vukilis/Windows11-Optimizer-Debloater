890bce8e0453b118b25a006386900430d142452c
vukilis <vuklekic22@gmail.com>
2024-01-16 21:45:52 +0100

    updated gaming preset

--- functions/public/installs/Invoke-ToggleGamingPreset.ps1
+++ functions/public/installs/Invoke-ToggleGamingPreset.ps1
@@ -18,8 +18,8 @@ function Invoke-ToggleGamingPreset {
             "Git", "Dotnet3", "Dotnet5", "Dotnet6",
             "Dotnet7", "vc2015_64", "vc2015_32", "Thorium",
             "Discord", "Eaapp", "Epicgames", "Steam",
-            "Ubisoft", "Greenshot", "Imageglass", "Obs",
-            "Notepadplus", "Sumatra", "7zip", "Cpuz",
+            "Ubisoft", "Greenshot", "Imageglass", "Obs", "Spotify",
+            "Vlc", "Notepadplus", "Sumatra", "7zip", "Cpuz",
             "ClasicMixer", "Hwinfo", "Msiafterburner", "Qbittorrent"
         ) -contains $checkBox.Name.Replace("DblInstall", "")){ $checkBox.IsChecked = $true }else{ $checkBox.IsChecked = $false }
 
