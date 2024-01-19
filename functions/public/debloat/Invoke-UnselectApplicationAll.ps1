7b82c9fa0f2a4481740ab7569b19e54a15da5ab4
vukilis <vuklekic22@gmail.com>
2024-01-19 01:44:23 +0100

    Invoke-UnselectApplicationAll.ps1

--- functions/public/debloat/Invoke-UnselectApplicationAll.ps1
+++ functions/public/debloat/Invoke-UnselectApplicationAll.ps1
@@ -6,5 +6,5 @@ function Invoke-UnselectApplicationAll {
     foreach ($app in $checkedCheckboxes) {
         $app.IsChecked = $false
     }
-    AddCustomLabel -content "Checked: 0 of $($matchingMsAppx.Count)" -panel $DblSelectPanel -Foreground "#F4AB51" -Margin @(15, 5, 15, 0) -FontSize 14 -FontWeight "Bold" -FontFamily "Gadugi"
+    $wpf_DblSelected.Content = "Selected: 0 of $($matchingMsAppx.Count)"
 }
