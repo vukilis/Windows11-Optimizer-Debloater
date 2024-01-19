19407760ad2e168dc6326b9d0c70213dc02eca62
vukilis <vuklekic22@gmail.com>
2024-01-19 01:38:38 +0100

    Invoke-Debloat.ps1

--- functions/public/debloat/Invoke-Debloat.ps1
+++ functions/public/debloat/Invoke-Debloat.ps1
@@ -1,10 +1,9 @@
 $DblGetPanel = $psform.FindName("GetDebloat")
-$DblSelectPanel = $psform.FindName("SetDebloat")
-AddCustomLabel -content "Checked: 0 of $($appx.Count)" -panel $DblSelectPanel -Foreground "#F4AB51" -Margin @(15, 5, 15, 0) -FontSize 14 -FontWeight "Bold" -FontFamily "Gadugi"
+$wpf_DblSelected.Content = "Selected: 0 of $($appx.Count)"
 
 # Iterate through each AppxPackage and create a TextBlock for each
 $matchingMsAppx = Invoke-MsAppxDebloat
 foreach ($app in $matchingMsAppx) {
     #Write-Host "ID: $($app.id), Name: $($app.name)"
-    AddCustomCheckBox -Id "$($app.Id)" -Name "$($app.Name)" -panel $DblGetPanel -Foreground "#a69f6c" -HorizontalAlignment "Left" -Cursor "Hand" -Margin @(15, 5, 15, 0) -FontSize 11 -FontFamily "Gadugi"
+    AddCustomCheckBox -Id "$($app.Id)" -Name "$($app.Name)" -panel $DblGetPanel -Foreground "#a69f6c" -HorizontalAlignment "Left" -Cursor "Hand" -Margin @(15, 10, 15, 5) -FontSize 11 -FontFamily "Gadugi"
 }
