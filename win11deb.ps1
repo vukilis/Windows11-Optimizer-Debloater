939ec5679be61b5d6c21a36edd133b3ff5b3ed86
vukilis <vuk.lekic@vukilis.com>
2025-08-16 23:43:25 +0200

    16.08.2025

--- win11deb.ps1
+++ win11deb.ps1
@@ -1991,7 +1991,7 @@ function Invoke-ToggleMegaPreset {
         [switch]$IsChecked  # Optional: allows forcing check/uncheck
     )
 
-    $path = ".\config\preset.json"
+    $path = "./config/preset.json"
     $sync = @{
         configs = @{
             preset = (Get-Content -Path $path -Raw | ConvertFrom-Json)
